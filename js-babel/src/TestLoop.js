cdab55d961cf910723ed85f1f91764c81802e2dc
Wayne Mao <wayne.mao@neulion.com.cn>
2021-10-08 14:39:16 +0800

    refactor(array-loop): let each loop to be execute separately

--- js-babel/src/TestLoop.js
+++ js-babel/src/TestLoop.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-constant-condition */
 const dataArr = [1, 2, 3, 4, 5];
 let dataArrResult = null;
 const dataObj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
@@ -19,157 +20,233 @@ const processItem = (item, index) => {
 };
 
 const dataArrSnapShot = JSON.stringify(dataArr);
-console.log(`dataArr: ${dataArrSnapShot}`);
 const dataObjSnapShot = JSON.stringify(dataObj);
-console.log(`dataObj: ${dataObjSnapShot}`);
 const dataMapSnapShot = JSON.stringify(Array.from(dataMap.entries()));
-console.log(`dataMap: ${dataMapSnapShot}`);
+
+const separator = '----------------------------------------------';
+let testPrepare = [separator];
+testPrepare.push('Initial Data:');
+testPrepare.push(`dataArr: ${dataArrSnapShot}`);
+testPrepare.push(`dataObj: ${dataObjSnapShot}`);
+testPrepare.push(`dataMap: ${dataMapSnapShot}`);
+testPrepare.push(separator);
+console.log(testPrepare.join('\r\n'));
 
 /*
 	Use Case: Execute something on every element, create a new element, and function chain is supported
 	Best Choice: Array.prototype.map()
  */
 // (1) loop
-for (let i = 0; i < dataArr.length; i++) {
-	dataArr[i] = processItem(dataArr[i]);
+if (false) {
+	for (let i = 0; i < dataArr.length; i++) {
+		dataArr[i] = processItem(dataArr[i]);
+	}
 }
 
-dataArr.forEach((item, index, items) => {
-	console.log(items.length);
-	result = processItem(item, index);
-});
-
-dataArr.forEach((item) => {
-	item = processItem(item); // immutable, doesn't work as expected
-});
-
-Object.keys(dataObj).forEach((key) => {
-	result = processItem(dataObj[key]);
-});
-Object.values(dataObj).forEach((value) => {
-	result = processItem(value);
-});
-Object.entries(dataObj).forEach(([key, value]) => {
-	result = processItem(dataObj[key]);
-	result = processItem(value);
-});
-
-for (const item in dataArr) {
-	result = processItem(item);
+if (false) {
+	dataArr.forEach((item, index, items) => {
+		console.log(items.length);
+		result = processItem(item, index);
+	});
 }
-for (let item in dataArr) {
-	item = processItem(item);
+
+if (false) {
+	dataArr.forEach((item) => {
+		item = processItem(item); // immutable, doesn't work as expected
+	});
 }
-for (const item in dataArr) {
-	if (dataArr.hasOwnProperty(item)) {
+
+if (false) {
+	Object.keys(dataObj).forEach((key) => {
+		result = processItem(dataObj[key]);
+	});
+}
+if (false) {
+	Object.values(dataObj).forEach((value) => {
+		result = processItem(value);
+	});
+}
+if (false) {
+	Object.entries(dataObj).forEach(([key, value]) => {
+		result = processItem(dataObj[key]);
+		result = processItem(value);
+	});
+}
+
+if (false) {
+	for (const item in dataArr) {
 		result = processItem(item);
 	}
 }
-for (let item in dataArr) {
-	if (dataArr.hasOwnProperty(item)) {
+if (false) {
+	for (let item in dataArr) {
+		item = processItem(item);
+	}
+}
+if (false) {
+	for (const item in dataArr) {
+		if (dataArr.hasOwnProperty(item)) {
+			result = processItem(item);
+		}
+	}
+}
+if (false) {
+	for (let item in dataArr) {
+		if (dataArr.hasOwnProperty(item)) {
+			item = processItem(item);
+		}
+	}
+}
+
+if (false) {
+	for (const item of dataArr) {
+		result = processItem(item);
+	}
+}
+if (false) {
+	for (let item of dataArr) {
 		item = processItem(item);
 	}
 }
 
-for (const item of dataArr) {
-	result = processItem(item);
+if (false) {
+	for (const key of Object.keys(dataObj)) {
+		result = processItem(key);
+	}
 }
-for (let item of dataArr) {
-	item = processItem(item);
+if (false) {
+	for (let key of Object.keys(dataObj)) {
+		key = processItem(key);
+	}
 }
-
-for (const key of Object.keys(dataObj)) {
-	result = processItem(key);
+if (false) {
+	for (const value of Object.values(dataObj)) {
+		result = processItem(value);
+	}
 }
-for (let key of Object.keys(dataObj)) {
-	key = processItem(key);
-}
-for (const value of Object.values(dataObj)) {
-	result = processItem(value);
-}
-for (const [key, index] of Object.entries(dataObj)) {
-	dataObj[key] = processItem(dataObj[key], index);
+if (false) {
+	for (const [key, index] of Object.entries(dataObj)) {
+		dataObj[key] = processItem(dataObj[key], index);
+	}
 }
 
 // (2) map (immutable)
-dataArrResult = dataArr.map((item) => item * 2);
-dataArr.map((item, index) => {
-	processItem(item, index);
-});
-dataArr.map(processItem);
+if (false) {
+	dataArrResult = dataArr.map((item) => item * 2);
+}
+if (false) {
+	dataArr.map((item, index) => {
+		processItem(item, index);
+	});
+}
+if (false) {
+	dataArr.map(processItem);
+}
 
 // (3) Array.from
-Array.from(dataMap, ([key, value]) => {
-	processItem(dataMap.get(key));
-	processItem(value);
-});
-Array.from(dataMap.keys(), (key) => {
-	processItem(dataMap.get(key));
-});
-Array.from(dataMap.values(), (value) => {
-	processItem(value);
-});
-Array.from(dataMap.entries(), ([key, value]) => {
-	processItem(dataMap.get(key));
-	processItem(value);
-});
-Array.from(Object.keys(dataObj), (key) => {
-	processItem(dataMap.get(key));
-});
-Array.from(Object.values(dataObj), (value) => {
-	processItem(value);
-});
-Array.from(Object.entries(dataObj), ([key, value]) => {
-	processItem(dataObj[key]);
-	processItem(value);
-});
+if (false) {
+	Array.from(dataMap, ([key, value]) => {
+		processItem(dataMap.get(key));
+		processItem(value);
+	});
+}
+if (false) {
+	Array.from(dataMap.keys(), (key) => {
+		processItem(dataMap.get(key));
+	});
+}
+if (false) {
+	Array.from(dataMap.values(), (value) => {
+		processItem(value);
+	});
+}
+if (false) {
+	Array.from(dataMap.entries(), ([key, value]) => {
+		processItem(dataMap.get(key));
+		processItem(value);
+	});
+}
+if (false) {
+	Array.from(Object.keys(dataObj), (key) => {
+		processItem(dataMap.get(key));
+	});
+}
+if (false) {
+	Array.from(Object.values(dataObj), (value) => {
+		processItem(value);
+	});
+}
+if (false) {
+	Array.from(Object.entries(dataObj), ([key, value]) => {
+		processItem(dataObj[key]);
+		processItem(value);
+	});
+}
 
 /*
 	Use Case: Finding a single element in the array
 	Best Choice: Array.prototype.find() / includes()
  */
 // (1) loop
-for (let i = 0; i < dataArr.length; i++) {
-	if (dataArr[i] === 3) {
-		result = dataArr[i];
-		break;
+if (false) {
+	for (let i = 0; i < dataArr.length; i++) {
+		if (dataArr[i] === 3) {
+			result = dataArr[i];
+			break;
+		}
 	}
 }
 
 // It loops every thing
-dataArr.forEach((item) => {
-	if (item === 3) {
-		result = item;
-	}
-});
+if (false) {
+	dataArr.forEach((item) => {
+		if (item === 3) {
+			result = item;
+		}
+	});
+}
 
-dataArr.some((item) => {
-	if (item === 3) {
-		result = item;
-		return true;
-	}
-	return false;
-});
-
-dataArr.every((item) => {
-	if (item === 3) {
-		result = item;
+if (false) {
+	dataArr.some((item) => {
+		if (item === 3) {
+			result = item;
+			return true;
+		}
 		return false;
-	}
-	return true;
-});
+	});
+}
 
-for (const item of dataArr) {
-	if (item === 3) {
-		result = item;
-		break;
+if (false) {
+	dataArr.every((item) => {
+		if (item === 3) {
+			result = item;
+			return false;
+		}
+		return true;
+	});
+}
+
+if (false) {
+	for (const item of dataArr) {
+		if (item === 3) {
+			result = item;
+			break;
+		}
 	}
 }
 
-result = dataArr.includes(3);
-result = dataArr.find((item) => item === 3);
-result = dataArr.findIndex((item) => item === 3);
-result = dataArr.filter((item) => item === 3).shift(); // not good, filter loop every item.
+if (false) {
+	result = dataArr.includes(3);
+}
+if (false) {
+	result = dataArr.find((item) => item === 3);
+}
+if (false) {
+	result = dataArr.findIndex((item) => item === 3);
+}
+if (false) {
+	result = dataArr.filter((item) => item === 3).shift(); // not good, filter loop every item.
+}
 
 /*
 	Process all element, until condition match
@@ -179,45 +256,63 @@ const maxCount = 10;
 let count = 0;
 
 // It loops every thing
-dataArr.forEach((item) => {
-	if (count < maxCount) {
-		processItem(item);
-	}
-});
+if (false) {
+	dataArr.forEach((item) => {
+		if (count < maxCount) {
+			processItem(item);
+		}
+	});
+}
 
-dataArr.some((item) => {
-	if (count < maxCount) {
-		processItem(item);
-		return true;
-	}
-	return false;
-});
+if (false) {
+	dataArr.some((item) => {
+		if (count < maxCount) {
+			processItem(item);
+			return true;
+		}
+		return false;
+	});
+}
 
 /*
 	Iterate over an array to count a property of each item
 	Best Choice: Array.prototype.reduce() or forEach() TODO: TBD
  */
-result = 0;
-result = dataArr.reduce((result, item) => result + item, 0);
-
-for (const item of dataArr) {
-	result += item;
+if (false) {
+	result = 0;
+	result = dataArr.reduce((result, item) => result + item, 0);
 }
 
-console.log(`dataArrResult: ${JSON.stringify(dataArrResult)}`);
-console.log(`dataObjResult: ${JSON.stringify(dataObjResult)}`);
-console.log(
+if (false) {
+	for (const item of dataArr) {
+		result += item;
+	}
+}
+
+let testResult = [separator];
+testResult.push('Test Result:');
+testResult.push(`result: ${result}`);
+testResult.push(`dataArrResult: ${JSON.stringify(dataArrResult)}`);
+testResult.push(`dataObjResult: ${JSON.stringify(dataObjResult)}`);
+testResult.push(
 	`dataMapResult: ${
 		dataMapResult ? JSON.stringify(Array.from(dataMapResult.entries())) : null
 	}`
 );
 if (JSON.stringify(dataArr) !== dataArrSnapShot) {
-	console.log(`dataArr changed`);
+	testResult.push('dataArr changed');
+} else {
+	testResult.push('dataArr no changes');
 }
 if (JSON.stringify(dataObj) !== dataObjSnapShot) {
-	console.log(`dataObj changed`);
+	testResult.push('dataObj changed');
+} else {
+	testResult.push('dataObj no changes');
 }
 if (JSON.stringify(Array.from(dataMap.entries())) !== dataMapSnapShot) {
-	console.log(`dataMap changed`);
+	testResult.push('dataMap changed');
+} else {
+	testResult.push('dataMap no changes');
 }
-console.log(`result: ${result}`);
+testResult.push(separator);
+console.log(testResult.join('\r\n'));
